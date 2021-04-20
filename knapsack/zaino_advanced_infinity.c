5bdd96ad5289651e4604d1d379e5c0e4c2611fe2
d0gs3c <calebgucciardi@gmail.com>
2021-04-20 13:01:48 +0200

    ottimizzazione

--- knapsack/zaino_advanced_infinity.c
+++ knapsack/zaino_advanced_infinity.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define NUM_OBJ 8
+#define NUM_OBJ 4
 #define MAX_WEIGHT 25
-
+#define max(a,b) ((a)>(b) ? (a) : (b) )
 
 
 // S T R U C T
@@ -15,14 +15,6 @@ struct obj{
 
 typedef struct obj Obj;
 
-
-// U T I L I T Y
-int max(int a,int b,int c){
-    if(a>b){
-        return (a>c) ? a:c;
-    }
-    return (b>c) ? b:c;
-}
 
 // M A I N
 void main(){
@@ -46,33 +38,14 @@ void main(){
     printf("\n"); 
 
     // B E S T - C H O I C E
-    int knapsack[NUM_OBJ][MAX_WEIGHT+1];
-    int multicopie, cp, singolacopia;
-    for(i=0;i<NUM_OBJ;i++){
-        for(j=0;j<=MAX_WEIGHT;j++){
-            // caso - base
-            if(i==0)
-                knapsack[i][j] = (shop[i].weight > j) ? 0:(shop[i].value*(j/shop[i].weight));
-            else{
-                if(shop[i].weight>j)
-                    knapsack[i][j] = knapsack[i-1][j];
-                else{
-                    // prima calcolo il caso in cui prendo multiocopie dell'elemento corrente
-                    // se rimane spazio aggiungo anche j-peso della riga precedente
-                    cp = j/shop[i].weight; // qunte cp (copie) posso prendere?
-                    multicopie = shop[i].value*cp;
-                    multicopie += (j-(shop[i].weight*cp))==0 ? 0:knapsack[i-1][j-(shop[i].weight*cp)];
-                    // adesso prendo solo una copia dell'elemento corrente e aggiungo valore della rica precedente j-peso
-                    singolacopia = shop[i].value+knapsack[i-1][j-shop[i].weight];
-                    // massimo tra multicopie, singolacopia e knapsack[i-1][j] (massimo raggiunto prima)
-                    knapsack[i][j] = max(knapsack[i-1][j],multicopie,singolacopia);
-                }
-            }
-        }
-    } 
+    static int knapsack[MAX_WEIGHT+1];
+    for(i=0;i<NUM_OBJ;i++)
+        for(j=0;j<=MAX_WEIGHT;j++)
+            knapsack[j] = (shop[i].weight > j) ? knapsack[j]:max(shop[i].value+knapsack[j-shop[i].weight],knapsack[j]);
+
 
     printf("MAX_WEIGHT::%d\n",MAX_WEIGHT);
-    printf("BEST_VALUE::%d\n",knapsack[NUM_OBJ-1][MAX_WEIGHT]);
+    printf("BEST_VALUE::%d\n",knapsack[MAX_WEIGHT]);
 
 }
 
