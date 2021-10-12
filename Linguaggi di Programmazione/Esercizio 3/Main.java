9668f034f445240f4dc7a8f3bc1c8a696e612d55
calebgucciardi <calebgucciardi@gmail.com>
2021-10-12 16:58:35 +0200

    Esercizio Regex

--- Linguaggi di Programmazione/Esercizio 3/Main.java
+++ Linguaggi di Programmazione/Esercizio 3/Main.java
@@ -1,4 +1,10 @@
+import java.util.*;
+
 public class Main{
+
+    public static boolean isValidEmail(String email){
+        return email.matches("\\w{1,}[@](\\w{1,}[.])+[a-z]{2,3}");
+    }
 
     public static String iniziali(String name, String surname){
         return name.charAt(0)+"&"+surname.charAt(0);
@@ -6,7 +12,7 @@ public class Main{
 
     public static String reverseString(String s){
         String result = "";
-        for(int i=s.length()-1;i>=0; --i)
+        for(int i=s.length()-1;i>=0;--i)
             result += s.charAt(i);
         return result;
     }
@@ -28,9 +34,18 @@ public class Main{
     }
 
     public static void main(String[] args){
-            System.out.println(iniziali("Ciao","Miao"));
+            /*System.out.println(iniziali("Ciao","Miao"));
             System.out.println(reverseString("Una mattina, mi sono alzato, o bella ciao, bella ciao, bella ciao ciao ciao"));
-            System.out.println(contaOccorrenze("aa aa aaa aaaa","aa"));
+            System.out.println(contaOccorrenze("aa aa aaa aaaa","aa"));*/
+
+            System.out.print(":: ");
+
+            Scanner scan = new Scanner(System.in);
+            String email = scan.nextLine();
+
+            System.out.println(isValidEmail(email));
+
+            scan.close();
     }
 
 }
