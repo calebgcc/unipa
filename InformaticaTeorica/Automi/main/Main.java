616c304bbe26f33464fd4edeb0fa887e1f6a2f66
calebgucciardi <calebgucciardi@gmail.com>
2021-12-12 12:21:57 +0100

    Refactoring

--- InformaticaTeorica/Automi/main/Main.java
+++ InformaticaTeorica/Automi/main/Main.java
@@ -4,13 +4,15 @@ public class Main {
     public static void main(String[] args){
         
         // DFA che riconosce L ={ {1,0}* : |0| = 2n, n>=0}
-        DFA q0 = new DFA("q0",true,true);
-        DFA q1 = new DFA("q1");
+        DFA[] q = {
+            new DFA("q0",true,true),
+            new DFA("q1")
+        };
 
-        q0.link('0', q1); q0.link('1', q0);
-        q1.link('0', q0); q1.link('1', q1);
+        q[0].link('0', q[1]); q[0].link('1', q[0]);
+        q[1].link('0', q[0]); q[1].link('1', q[1]);
 
-        DFA.isAccepted(q0, "10110010");
+        DFA.isAccepted(q[0], "10110010");
 
         // DFA che riconosce L = { {a,b,c}* : dopo ogni 'a' vi sia 'bb' }
         DFA uno = new DFA("q",true,true);
