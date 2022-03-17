48f338f0497c5c1a81503443800c60d01b4b4047
calebgucciardi <calebgucciardi@gmail.com>
2022-03-17 17:55:09 +0100

    added iterator features to Jlist

--- AlgoritmiStruttureDati/jalgo/src/jalgo/Main.java
+++ AlgoritmiStruttureDati/jalgo/src/jalgo/Main.java
@@ -1,21 +1,48 @@
 package jalgo;
+
 import jalgo.datastructures.Jlist;
 
 public class Main{
     public static void main(String[] args){ 
 
         Jlist<Integer> list = new Jlist<>();
+        
+        for(int i=0;i<20;++i)
+            list.append(i);
 
-        System.out.println(list);
+        list.set(0);
 
-        list.append(3);
-        list.append(2);
-        list.append(-1);
+        for(int i=0; i<5; ++i){
+            System.out.print(list.get()+" ");
+            list.next();
+            System.out.println(list.hasNext());
+        }
 
-        System.out.println(list);
+        list.set(12);
 
+        for(int i=0; i<5; ++i){
+            System.out.print(list.get()+" ");
+            list.prev();
+            System.out.println(list.hasPrev());
+        }
 
+        list.set(0);
+        while(list.hasNext()){
+            System.out.print(list.get()+" ");
+            list.next();
+        }
+        System.out.println(list.get());
 
+        while(list.hasPrev()){
+            System.out.print(list.get()+" ");
+            list.prev();
+        }
+        System.out.println(list.get());
+
+        while(list.isSet()){
+            System.out.print(list.get()+" ");
+            list.next();
+        }
 
     }
 }
--- AlgoritmiStruttureDati/jalgo/src/jalgo/datastructures/Jlist.java
+++ AlgoritmiStruttureDati/jalgo/src/jalgo/datastructures/Jlist.java
@@ -15,13 +15,65 @@ public class Jlist<T> {
 
     private Node head;
     private Node tail;
+    private Node iterator;
     private int size;
 
     public Jlist(){
         this.head = null;
         this.tail = null;
+        this.iterator = null;
         this.size = 0;
     }
+
+    public void set(int index) throws IndexOutOfBoundsException{
+        if(index>=size || index<0)
+            throw new IndexOutOfBoundsException();
+        
+        Node temp;
+        if(index > (size/2)){
+            temp = tail;
+            for(int i=size-1;i>index;--i)
+                temp = temp.prev;
+            iterator = temp;
+        }
+        else{
+            temp = head;
+            for(int i=0;i<index;++i)
+                    temp = temp.next;
+            iterator = temp;
+        }
+    }
+
+    public boolean hasNext(){
+        return iterator==null ? false:(iterator.next != null);
+    }
+
+    public boolean hasPrev(){
+        return iterator==null ? false:(iterator.prev != null);
+    }
+
+    public boolean isSet(){
+        return iterator!=null;
+    }
+
+    public void next() throws RuntimeException{
+        if(iterator == null)
+            throw new RuntimeException();
+        iterator = iterator.next;
+    }
+
+    public void prev() throws RuntimeException{
+        if(iterator == null)
+            throw new RuntimeException();
+        iterator = iterator.prev;
+    }
+
+    public T get() throws RuntimeException{
+        if(iterator == null)
+            throw new RuntimeException();
+        return iterator.value;
+    }
+
 
     public void append(T value){
         if(head == null){
@@ -39,6 +91,69 @@ public class Jlist<T> {
         size++;
     }
 
+    public void remove(int index) throws IndexOutOfBoundsException{
+        if(index>=size || index<0)
+            throw new IndexOutOfBoundsException();
+
+        if(index == 0){
+            size--;
+            iterator = iterator==head ? null:iterator;
+            head = head.next;
+            if(head != null) // if the head was the only node
+                head.prev = null;
+            return;
+        }
+        else if(index == (size-1)){
+            size--;
+            iterator = iterator==tail ? null:iterator;
+            tail = tail.prev;
+            tail.next = null;
+            return;
+        }
+        
+        Node temp;
+        if(index >= (size/2)){
+            temp = tail;
+            for(int i=size-1; i>index; --i)
+                temp = temp.prev;
+        }
+        else{
+            temp = head;
+            for(int i=0; i<index; ++i)
+                temp = temp.next;
+        }    
+        size--;
+        iterator = iterator==temp ? null:iterator;
+        (temp.prev).next = temp.next; // next of the previous is equal to temp.next
+        (temp.next).prev = temp.prev; // previous of the next is equal to temp.prev
+    }
+
+    public T get(int index)throws IndexOutOfBoundsException{
+        if(index>=size || index<0)
+            throw new IndexOutOfBoundsException();
+        
+        if(index > (size/2)){
+            Node temp = tail;
+            for(int i=size-1;i>index;--i)
+                temp = temp.prev;
+            return temp.value;
+        }
+    
+        Node temp = head;
+        for(int i=0;i<index;++i)
+                temp = temp.next;
+        return temp.value;
+    }
+
+    public boolean isEmpty(){
+        return size==0;
+    }
+
+    public int size(){
+        return size;
+    }
+
+
     @Override
     public String toString(){
         if(size == 0)
@@ -54,6 +169,5 @@ public class Jlist<T> {
         builder.append("]");
         return builder.toString();
     }
-
 
 }
