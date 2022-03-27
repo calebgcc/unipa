b9701a785f0d9188064e041a124e6d0bc8ced4e9
calebgucciardi <calebgucciardi@gmail.com>
2022-03-27 12:29:56 +0200

    tested Jqueue and Jstack

--- AlgoritmiStruttureDati/jalgo/src/jalgo/Main.java
+++ AlgoritmiStruttureDati/jalgo/src/jalgo/Main.java
@@ -1,15 +1,27 @@
 package jalgo;
 
+import java.util.List;
+
+import jalgo.datastructures.Jqueue;
 import jalgo.datastructures.Jstack;
 
 public class Main{
     
     public static void main(String[] args){
-        Jstack<Integer> stack = new Jstack<>();
-        for(int i=0; i<10; ++i){
-            stack.push(i);
-            System.out.println(stack.pop());
-        }
+        Jqueue<String> coda = new Jqueue<>(List.of("a","b","c","d"));
+        Jstack<Integer> stack = new Jstack<>(new Integer[]{1,2,3,4,5,6,7,8,9,10}); 
+
         System.out.println(stack);
+        System.out.println(coda);
+
+        while(!stack.isEmpty())
+            System.out.print(stack.pop()+" ");
+
+        System.out.println();
+
+        while(!coda.isEmpty())
+            System.out.print(coda.pop()+" ");
+
+        System.out.println();
     }
 }
